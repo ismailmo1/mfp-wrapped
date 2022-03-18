a13d88a2024fe6f65ea912351104036237452696
ismail <ismailmo4@gmail.com>
2022-03-18 23:50:18 +0000

    added data scraping to app

--- app/app.py
+++ app/app.py
@@ -1,63 +1,50 @@
-import time
+import os
 from datetime import date
 
 import pandas as pd
 import streamlit as st
+from dotenv import load_dotenv
+
+from myfitnesspal import diary_scraping
+
+load_dotenv()
 
 st.title("Seven years of Ismail trying not be fat")
 
 prog_bar = st.progress(0)
 
 
-@st.cache
 def load_mfp_data(start_date: date, end_date: date):
-    # code taken from food eda notebook
-    food_df = pd.read_csv("myfitnesspal/mfp-extract-2021.csv", index_col=0)
-    # reset index so each line has unique index (i.e. indices not
-    # restarted from 0 for each new day)
-    food_df.reset_index(drop=True, inplace=True)
-    # drop all rows containing column headers are repeated
-    # (i.e. where dataframe is appended)
-    food_df.drop(food_df[food_df["food"] == "food"].index, inplace=True)
-    macro_cols = [
-        "carbs_g",
-        "fat_g",
-        "protein_g",
-        "goal_carbs_g",
-        "goal_fat_g",
-        "goal_protein_g",
-    ]
-    food_df[macro_cols] = food_df[macro_cols].applymap(
-        lambda x: x.split("  ")[0]
-    )
+    diary_df = pd.DataFrame()
+    progress = 0
+    for df in diary_scraping.get_diary_data(
+        start_date,
+        end_date,
+        public=False,
+        user=os.environ["MFP_USER"],
+        pwd=os.environ["MFP_PASS"],
+    ):
+        progress += 1
+        prog_bar.progress(progress)
+        diary_df = pd.concat([diary_df, df], axis=0, join="outer")
 
-    # convert datatypes
-    numeric_cols = [
-        col for col in food_df.columns if "_g" in col or "calories" in col
-    ]
-    food_df[numeric_cols] = food_df[numeric_cols].apply(pd.to_numeric)
-    food_df["date"] = pd.to_datetime(food_df["date"])
-
-    return food_df
+    return diary_df
 
 
-for i in range(0, 101):
-    prog_bar.progress(i)
-    time.sleep(0.01)
+diary_df = load_mfp_data(date(2021, 1, 1), date(2021, 1, 10))
+st.write(diary_df)
 
-df_21 = load_mfp_data(date(2021, 1, 1), date(2021, 12, 31))
 
-st.write(df_21)
+# st.write(df_21)
+# col_1, col_2, col_3, col_4 = st.columns(4)
 
-col_1, col_2, col_3, col_4 = st.columns(4)
+# col_1.metric("Calories", df_21["calories_kcal"].sum(), delta="100%")
+# col_2.metric("Carbs", df_21["carbs_g"].sum(), delta="100%")
+# col_3.metric("Fats", df_21["fat_g"].sum(), delta="100%")
+# col_4.metric("Protein", df_21["protein_g"].sum(), delta="100%")
 
-col_1.metric("Calories", df_21["calories_kcal"].sum(), delta="100%")
-col_2.metric("Carbs", df_21["carbs_g"].sum(), delta="100%")
-col_3.metric("Fats", df_21["fat_g"].sum(), delta="100%")
-col_4.metric("Protein", df_21["protein_g"].sum(), delta="100%")
+# st.header("most common food entries")
 
-st.header("most common food entries")
-
-st.bar_chart(
-    df_21.groupby("food").count()["date"].sort_values(ascending=False)[0:10]
-)
+# st.bar_chart(
+#     df_21.groupby("food").count()["date"].sort_values(ascending=False)[0:10]
+# )
---
+++ app/myfitnesspal/diary_scraping.py
@@ -0,0 +1,150 @@
+import json
+from datetime import date, timedelta
+
+import pandas as pd
+import requests
+from requests import Session
+
+
+def login_mfp(username: str, password: str) -> Session:
+    """login user to myfitnesspal
+
+    Args:
+        username (str): username to login
+        password (str): password for login
+
+    Raises:
+        Exception: if "account/logout" isn't found in response text which
+        would've indicated a successful login
+
+    Returns:
+        Session: logged in user session
+    """
+    # login to myfitness pal
+    session = requests.session()
+
+    # grab csrf token
+    csrf_res = session.get("https://www.myfitnesspal.com/api/auth/csrf")
+    csrf_token = json.loads(csrf_res.text)["csrfToken"]
+
+    # send credentials
+    session.post(
+        "https://www.myfitnesspal.com/api/auth/callback/credentials?",
+        data={
+            "username": username,
+            "password": password,
+            "csrfToken": csrf_token,
+            "callbackUrl": "https://www.myfitnesspal.com/account/login",
+            "redirect": "false",
+            "json": "true",
+        },
+        headers={"content-type": "application/x-www-form-urlencoded"},
+    )
+
+    # get session
+    session.get("https://www.myfitnesspal.com/api/auth/session")
+
+    # check if login successful
+    res = session.get("https://www.myfitnesspal.com/")
+
+    if res.text.find("/account/logout") > 0:
+        print("logged in successfully!")
+        return session
+    else:
+        raise Exception("login failed!")
+
+
+def clean_mfp_extract(df: pd.DataFrame) -> pd.DataFrame:
+    # cleanup column names
+    df.columns = [
+        "food",
+        *[str(col).lower().replace("  ", "_") for col in df.iloc[0][1:]],
+    ]
+    df.drop(0, inplace=True)
+
+    # remove junk rows with no food data
+    non_food_row_idx = df[
+        df["food"].apply(
+            lambda x: "Add Food  Quick Tools  Quick add calories" in str(x)
+        )
+    ].index
+    meal_title_row_idx = df[df["food"].apply(lambda x: len(str(x)) == 1)].index
+    non_food_row_idx = non_food_row_idx.append(meal_title_row_idx)
+    df.drop(non_food_row_idx, inplace=True)
+    df.dropna(axis=(0), how="all", inplace=True)
+    df.dropna(axis=(1), how="all", inplace=True)
+
+    # add daily goal columns
+    daily_goals_df = df[df["food"] == "Your Daily Goal"]
+    new_cols = ["food"]
+
+    # name goals with prefix "goal_"
+    for col in daily_goals_df.columns[1:]:
+        new_cols.append("goal_" + str(col).split(" ")[0].lower())
+
+    daily_goals_df.columns = new_cols
+
+    # add daily goals columsn to df
+    new_df = pd.concat([df, daily_goals_df])
+    new_df.fillna(method="bfill", inplace=True)
+
+    # drop last 5 rows - non food items
+    cleaned_df = new_df.drop(new_df.index[-5:]).reset_index(drop=True)
+    return cleaned_df
+
+
+def extract_diary(logged_in_mfp_session: Session, date: date) -> pd.DataFrame:
+    """extract dataframe of food diary from date.
+
+    Args:
+        logged_in_mfp_session (Session): authenticated session on myfitnesspal
+        (use login_mfp)
+        date (date): date to extract food diary from
+
+    Raises:
+    Exception: if no tables found in diary html
+
+    Returns:
+        pd.DataFrame: food diary table extracted from html page into dataframe
+        dataframe is cleaned so each row is 1 food entry
+        date is added as an extra column to the dataframe
+        all other data (macro targets, totals etc) is added as a column.
+    """
+    date_param = f"date={date.isoformat()}"
+    res = logged_in_mfp_session.get(
+        f"https://www.myfitnesspal.com/food/diary?{date_param}"
+    )
+    try:
+        df = pd.read_html(res.text, flavor="lxml")[0]
+        clean_df = clean_mfp_extract(df)
+        clean_df["date"] = date
+        return clean_df
+    except ValueError:
+        raise Exception(ValueError.__repr__())
+
+
+def get_diary_data(
+    start_date: date,
+    end_date: date,
+    public: bool = True,
+    user: str = None,
+    pwd: str = None,
+) -> pd.DataFrame:
+    # load secrets from .env
+
+    if not public:
+        if user and pwd:
+            # create session
+            mfp_session = login_mfp(user, pwd)
+        else:
+            raise Exception(
+                "You must provide a username and password for private diaries"
+            )
+
+    # df = pd.DataFrame()
+    while start_date < end_date:
+        print(f"extracting diary for {start_date}")
+        diary_df = extract_diary(mfp_session, start_date)
+        # pd.concat([df, diary_df], axis=0, join="outer")
+        start_date += timedelta(days=1)
+        yield diary_df
