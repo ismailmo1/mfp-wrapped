5fd22e022fcecaa46ca3c0b24607a180640662cd
ismail <ismailmo4@gmail.com>
2022-03-18 00:05:24 +0000

    added freq chart to app

--- app.py
+++ app.py
@@ -11,13 +11,44 @@ prog_bar = st.progress(0)
 
 @st.cache
 def load_mfp_data(start_date: date, end_date: date):
-    df = pd.read_csv("myfitnesspal/mfp-extract-2021.csv", index_col=0)
-    return df
+    # code taken from food eda notebook
+    food_df = pd.read_csv("myfitnesspal/mfp-extract-2021.csv", index_col=0)
+    # reset index so each line has unique index (i.e. indices not
+    # restarted from 0 for each new day)
+    food_df.reset_index(drop=True, inplace=True)
+    # drop all rows containing column headers are repeated
+    # (i.e. where dataframe is appended)
+    food_df.drop(food_df[food_df["food"] == "food"].index, inplace=True)
+    macro_cols = [
+        "carbs_g",
+        "fat_g",
+        "protein_g",
+        "goal_carbs_g",
+        "goal_fat_g",
+        "goal_protein_g",
+    ]
+    food_df[macro_cols] = food_df[macro_cols].applymap(
+        lambda x: x.split("  ")[0]
+    )
+
+    # convert datatypes
+    numeric_cols = [
+        col for col in food_df.columns if "_g" in col or "calories" in col
+    ]
+    food_df[numeric_cols] = food_df[numeric_cols].apply(pd.to_numeric)
+    food_df["date"] = pd.to_datetime(food_df["date"])
+
+    return food_df
 
 
 for i in range(0, 101):
     prog_bar.progress(i)
     time.sleep(0.01)
 
+df_21 = load_mfp_data(date(2021, 1, 1), date(2021, 12, 31))
+st.write(df_21)
 
-st.write(load_mfp_data(date(2021, 1, 1), date(2021, 12, 31)))
+st.header("most common food entries")
+st.bar_chart(
+    df_21.groupby("food").count()["date"].sort_values(ascending=False)[0:10]
+)
