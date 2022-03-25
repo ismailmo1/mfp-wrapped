5ac68a16aa86a100384ac155dc1c6ce9f6024a68
Ismail Mohammed <ismailmo4@gmail.com>
2022-03-25 22:35:05 +0000

    added pseudo routing

--- app/main.py
+++ app/main.py
@@ -1,17 +1,85 @@
+"""
+Entry point for streamlit app
+"""
 from datetime import datetime, timedelta
 
 import streamlit as st
 
-from app_utils import load_mfp_data, show_metrics
-from myfitnesspal.analysis import (
+from app.app_utils import load_mfp_data, show_metrics
+from app.myfitnesspal.analysis import (
     plot_macro_treemap,
     plot_most_common,
     total_logged_days,
     total_macros,
 )
 
-st.set_page_config("Wrapped", page_icon="burrito.png", layout="wide")
 
+def run_analysis():
+    """
+    main function to scrape mfp diaries and analyse data
+    """
+
+    st.session_state["diary_df"] = load_mfp_data(
+        start_date, end_date, mfp_user
+    ).copy()
+    diary_df = st.session_state["diary_df"]
+
+    st.metric(
+        "Total days logged",
+        f"{total_logged_days(diary_df)}/{(end_date-start_date).days +1}",
+    )
+
+    st.header("Totals")
+    show_metrics(total_macros(diary_df))
+    st.plotly_chart(plot_most_common(diary_df), use_container_width=True)
+
+    st.radio(
+        "View breakdown for:",
+        ["all", "carbs", "fat", "protein"],
+        key="selected_macro",
+    )
+    st.plotly_chart(
+        plot_macro_treemap(diary_df, st.session_state["selected_macro"]),
+        use_container_width=True,
+    )
+    # radio button css
+    st.write(
+        """<style>
+        div.row-widget.stRadio > div{flex-direction:row;}
+        .st-dw{padding-right:5%};
+        </style>""",
+        unsafe_allow_html=True,
+    )
+
+
+def intial_page_load():
+    """
+    run on initial page load
+    """
+    st.session_state["welcomed"] = True
+    st.session_state["selected_macro"] = "all"
+    starter_msg = st.empty()
+    starter_img = st.empty()
+
+    starter_msg.markdown(
+        """Inspired by Spotify's [Wrapped]\
+    (https://en.wikipedia.org/wiki/Spotify_Wrapped) marketing campaign
+
+    ← Enter your mfp username in the sidebar, make your diary public and
+    let's see what you've been eating!"""
+    )
+
+    with starter_img.expander("Preview"):
+        st.image("app/himym.jpg", caption="what you can look forward to")
+
+    if start_btn:
+        starter_msg.empty()
+        starter_img.empty()
+        run_analysis()
+
+
+st.set_page_config("Wrapped", page_icon="app/burrito.png", layout="wide")
+st.title("myfitnesspal wrapped 🌯")
 with st.sidebar:
     mfp_user = st.text_input(
         "myfitnesspal username", "ismailmo", placeholder="username"
@@ -29,32 +97,8 @@ with st.sidebar:
 
     start_btn = st.button("Get Data")
 
-st.title("myfitnesspal wrapped 🌯")
-
-starter_msg = st.empty()
-starter_img = st.empty()
-
-starter_msg.markdown(
-    """Inspired by Spotify's [Wrapped]\
-(https://en.wikipedia.org/wiki/Spotify_Wrapped) marketing campaign
-
-← Enter your mfp username in the sidebar, make your diary public and
-let's see what you've been eating!"""
-)
-
-with starter_img.expander("Analysis awaits!"):
-    st.image("himym.jpg", caption="what you can look forward to")
-
-if start_btn:
-    starter_msg.empty()
-    starter_img.empty()
-    diary_df = load_mfp_data(start_date, end_date, mfp_user)
-    st.metric(
-        "Total days logged",
-        f"{total_logged_days(diary_df)}/{(end_date-start_date).days +1}",
-    )
-
-    st.header("Totals")
-    show_metrics(total_macros(diary_df))
-    st.plotly_chart(plot_most_common(diary_df), use_container_width=True)
-    st.plotly_chart(plot_macro_treemap(diary_df), use_container_width=True)
+if "welcomed" not in st.session_state:
+    intial_page_load()
+else:
+    # run analysis if welcome page already viewed
+    run_analysis()
