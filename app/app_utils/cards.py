9ff96798b3d259362c4d713d3c13443e4218625b
Ismail Mohammed <ismailmo4@gmail.com>
2022-12-07 16:12:28 +0000

    fix card formatting

--- app/app_utils/cards.py
+++ app/app_utils/cards.py
@@ -38,10 +38,27 @@ def create_base_card(
 
 def generate_total_kcal_card(num_kcal: int):
     draw, card = create_base_card((148, 240, 180))
-    width, height = card.size
-    title_fnt = ImageFont.truetype(FONT_PATH, 90)
+    card_width, card_height = card.size
+    font_size = 90
+    title_fnt = ImageFont.truetype(
+        "/home/ismail/.local/share/fonts/GothamBold.ttf", font_size
+    )
+    _, _, num_kcal_w, _ = draw.textbbox(
+        (0, 0), f"{num_kcal:,}", font=title_fnt
+    )
+
+    # shirnk font size for large total_kcal displays
+    while num_kcal_w > card_width - 20:
+        font_size -= 3
+        title_fnt = ImageFont.truetype(
+            "/home/ismail/.local/share/fonts/GothamBold.ttf", font_size
+        )
+        _, _, num_kcal_w, _ = draw.textbbox(
+            (0, 0), f"{num_kcal:,}", font=title_fnt
+        )
+
     draw.text(
-        (5, (height / 2) - 180),
+        (5, (card_height / 2) - 180),
         f"{num_kcal:,}",
         font=title_fnt,
         fill=(0, 0, 0),
@@ -55,30 +72,66 @@ def generate_total_kcal_card(num_kcal: int):
     household_daily_usage_kcal = 25_000
     num_homes = num_kcal / household_daily_usage_kcal
     draw.text(
-        (200, (height / 2) - 100),
+        (200, (card_height / 2) - 100),
         "Total calories",
         font=kcal_fnt,
         fill=(110, 110, 110),
     )
 
     draw.text(
-        (20, (height / 2) - 75),
+        (20, (card_height / 2) - 75),
         f"Enough to power " f"\n{num_homes:.2f} average UK\nhomes for a day",
         font=fnt,
     )
 
     # add home icons
     home_img = Image.open("../app/images/home.png")
-    home_img.thumbnail((100, 80))
+    home_imgs_top = 380
+    num_cols = 4
+    num_rows = math.ceil(num_homes / num_cols)
+    home_img_width = 80
+    w_pad = 3
+    total_width = (home_img_width + w_pad) * num_cols
+    total_height = (home_img_width * num_rows) + home_imgs_top
+    home_img.thumbnail((home_img_width, home_img_width))
+
+    avail_height = card_height - home_imgs_top - 50
+    avail_width = card_width - 20
+
+    height_clash = total_height > avail_height
+    width_clash = total_width > avail_width
+
+    while height_clash or width_clash:
+        if height_clash:
+            # increasing cols wont help for width clash
+            num_cols += 1
+
+        max_width_per_home = avail_width / num_cols
+        home_img_width = int(max_width_per_home - w_pad)
+        num_rows = math.ceil(num_homes / num_cols)
+        total_width = (home_img_width + w_pad) * num_cols
+        total_height = home_img_width * num_rows
+        home_img.thumbnail((home_img_width, home_img_width))
+        height_clash = total_height > avail_height
+        width_clash = total_width > avail_width
 
     home_imgs = [home_img for _ in range(math.ceil(num_homes))]
     fraction = num_homes % 1
     if fraction > 0:
-        width = home_img.size[0]
-        home_imgs[-1] = home_img.crop((0, 0, int(width * fraction), 100))
-    # TODO: arrange grid and resize based on num home_imgs
+        card_width = home_img.size[0]
+        home_imgs[-1] = home_img.crop((0, 0, int(card_width * fraction), 100))
+
     for idx, home in enumerate(home_imgs):
-        card.paste(home, (20 + (80 * idx), 420), home)
+        row = idx // num_cols
+        col = idx % num_cols
+        card.paste(
+            home,
+            (
+                20 + (home_img_width * col),
+                (row * (w_pad + home_img_width)) + home_imgs_top,
+            ),
+            home,
+        )
 
     return card
 
@@ -124,7 +177,7 @@ def generate_days_tracked_card(
 
     perc_days = tracked_days / total_days
 
-    perc_font = ImageFont.truetype(FONT_PATH, 150)
+    perc_font = ImageFont.truetype(FONT_PATH, 140)
     draw.text(
         (0, 120), f"{perc_days*100:.0f}%", font=perc_font, fill=(0, 0, 0, 200)
     )
@@ -147,8 +200,9 @@ def generate_days_tracked_card(
     return days_tracked_card
 
 
-def generate_adherence_card(adherence: float):
+def generate_adherence_card(adherence: float, tolerance: float = 0.1):
     draw, adherence_card = create_base_card((0, 102, 238))
+    card_w, card_h = adherence_card.size
 
     phrase_imgs = (
         ("None of my business though", "../app/images/kermit.jpg"),
@@ -167,16 +221,22 @@ def generate_adherence_card(adherence: float):
         return 1
 
     threshold_lvl = get_adherence_level(adherence)
+    phrase_font = ImageFont.truetype(FONT_PATH, 18)
 
     meme_img = Image.open(phrase_imgs[threshold_lvl][1])
     meme_img.thumbnail((180, 180))
     meme_img_border = ImageOps.expand(meme_img, border=5, fill=(0, 0, 0))
-    adherence_card.paste(meme_img_border, (90, 80))
-    phrase_font = ImageFont.truetype(FONT_PATH, 18)
+    meme_w, meme_h = meme_img_border.size
+    img_top = 90
+    adherence_card.paste(
+        meme_img_border, (int((card_w - meme_w) / 2), img_top)
+    )
 
+    phrase = phrase_imgs[threshold_lvl][0]
+    _, _, w, _ = draw.textbbox((0, 0), phrase, font=phrase_font)
     draw.text(
-        (80, 270),
-        phrase_imgs[threshold_lvl][0],
+        ((card_w - w) / 2, img_top + meme_h + 10),
+        phrase,
         font=phrase_font,
         align="center",
     )
@@ -189,6 +249,10 @@ def generate_adherence_card(adherence: float):
     )
 
     disclaimer_font = ImageFont.truetype(FONT_PATH, 15)
-    draw.text((20, 550), "*within 5% of kcal goal", font=disclaimer_font)
+    draw.text(
+        (20, 550),
+        f"*within {tolerance*100:.0f}% of kcal goal",
+        font=disclaimer_font,
+    )
 
     return adherence_card
